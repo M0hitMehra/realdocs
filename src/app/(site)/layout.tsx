2a6af7e5b4eda7a51e76dbd473a4fc3b4480c547
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-01-11 21:59:57 +0530

    start of project

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const HomePageLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main>
+            {children}
+        </main>
+    )
+}
+
+export default HomePageLayout
