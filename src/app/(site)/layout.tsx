300e2c5c06597d25881353ac3f48edcca5d03d3e
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-01-23 23:04:24 +0530

    Building navigation menu

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+import Header from '@/components/landing-page/header'
 import React, { ReactNode } from 'react'
 
 const HomePageLayout = ({ children }: { children: ReactNode }) => {
     return (
         <main>
+            <Header />
             {children}
         </main>
     )
