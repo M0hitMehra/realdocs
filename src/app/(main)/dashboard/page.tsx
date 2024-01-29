be0a68190f931529f2977d2be74b89502fc41260
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-01-29 23:20:30 +0530

    dashboard setup

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,32 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import React from 'react'
+import { cookies } from "next/headers"
+import db from '@/lib/supabase/db'
+import { redirect } from 'next/navigation'
+import DashboardSetup from '@/components/dashboard-setup/dashboard-setup'
 
-const Page = () => {
+const DashboardPage = async () => {
+    const supabase = createServerComponentClient({ cookies })
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if (!user) return
+
+    const workspace = await db.query.workspaces.findFirst({
+        where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id)
+    })
+
+    if (!workspace) {
+        return (
+            <div className=' bg-background w-screen h-screen flex justify-center items-center ' >
+                <DashboardSetup />
+            </div>
+        )
+    }
+    redirect(`/dashboard/${workspace.id}`)
+
     return (
-        <div>Page</div>
+        <div>DashboardPage</div>
     )
 }
 
-export default Page
+export default DashboardPage
