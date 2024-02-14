43084eb7bfde5a0c5157ed542efcd37c723efd32
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-02-14 23:31:59 +0530

    WorkSpace Sidebar

--- src/lib/supabase/queries.ts
+++ src/lib/supabase/queries.ts
@@ -46,3 +46,13 @@ export const getFiles = async (folderId: string) => {
         return { data: null, error: 'Error' };
     }
 };
+
+export const getFolders = async (workspaceId: string) => {
+    const isValid = validate(workspaceId)
+    if (!isValid) return { data: null, error: null }
+    try {
+        const results: Folder[] | [] = await db.select().from(folders).orderBy(folders.createdAt).where(eq(folders.workspaceId, workspaceId))
+    } catch (error) {
+        return { data: null, error: "Error" }
+    }
+}
